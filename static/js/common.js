900895d8d4d2370d4fdec40d50f497eb7e733177
Saurav Biswas <saurav1124@gmail.com>
2013-04-11 07:46:17 -0400

    url manupulation

--- static/js/common.js
+++ static/js/common.js
@@ -1,4 +1,6 @@
 var jkCommon = new (function() {
+  this.hashLeftNavLoaded = false;
+  this.hashTrackLoaded = false;
   this.init = function() {
     $('.tipt').tooltip( {placement: "top", html: true, container: 'body' });
     $('.tipr').tooltip( {placement: "right", html: true, container: 'body'});
@@ -27,5 +29,100 @@ var jkCommon = new (function() {
     $(".topfsh").text(msg);
     $(".topfsh").slideDown();
     setTimeout(function() { $(".topfsh").slideUp(); }, 5000 );
+  };
+  this.addHash = function(idx, str) {
+  	var currHash = location.hash.split('!');
+  	var newHash = new Array();
+  	for (i=0;i<=idx; i++) {
+  		if (i == idx) {
+  			newHash[i] = str;
+  		} else if (currHash[i+1] != undefined) {
+  			newHash[i] = currHash[i+1];
+  		} else {
+  			newHash[i] = "";
+  		}
+  	}
+  	if (currHash.length-1 > idx+1) {
+  		for (i=idx+1;i<currHash.length-1;i++) {
+  			newHash[i] = currHash[i+1];
+  		}
+  	}
+  	var h = "";
+  	for (var i=0;i<newHash.length;i++) {
+    	h += "!" + newHash[i];
+  	}
+  	window.location.hash = h;
+  };
+  this.applyHash = function() {
+  	if (!this.hashLeftNavLoaded) {
+	  	var hashes = location.hash.split('!');
+	  	for (var i=1;i<hashes.length;i++) {
+	  		if (i == 1 && hashes[i].substring(1) != "") {
+	  			$("#ln" + hashes[i].substring(1)).click();
+	  		} else if (i == 2 && hashes[i] != "") {
+	  			$("#tk" + hashes[i]).click();
+	  		}
+	  	}
+  	}
+  	this.hashLeftNavLoaded = true;
+  };
+  this.loadHashLeftNav = function() {
+    if (!this.hashLeftNavLoaded) {
+      var hashes = location.hash.split('!');
+      if (hashes.length > 1) {
+        if (hashes[1] != "") {
+          $("#ln" + hashes[1]).click();
+        }
+      }
+    }
+    this.hashLeftNavLoaded = true;
+  };
+  this.playHashTrack = function() {
+    if (!this.hashTrackLoaded) {
+      var hashes = location.hash.split('!');
+      if (hashes.length > 2) {
+        this.playTrack(hashes[2]);
+      }
+    }
+    this.hashTrackLoaded = true;
+  };
+  this.playTrack = function(id) {
+    this.destroyTrackPopover();
+  	var track = $("#tk" + id);
+    if (track != undefined) {
+      if (track.attr("data-playing") == undefined) {
+      var id = track.attr("data-id");
+      var detailUrl = track.attr("data-url");
+      $.ajax({
+        url: detailUrl,
+        success: function(data) {
+          $(".player").attr("data-track", id);
+          $(".player .dtls .ttl").text(data.title);
+          $(".player .dtls .info").text(data.artist + " - " + data.album);
+          $(".player .art img").attr("src", data.artwork.url);
+
+          track.closest("ul").find("li").removeClass("playing");
+          track.closest("li").addClass("playing");
+          $(".play-track").removeAttr("data-playing");
+          track.attr("data-playing", "1");
+          track.closest("li").find(".play-count").text(data.play_count);
+          track.closest("li").find(".play-count").show();
+          jkCommon.addHash(1, id);
+
+          var url = data.media.mp3;
+          $(".ml-no-play").fadeOut("fast");
+          $(".ml-play").fadeIn("fast");
+          $("#player_1").jPlayer("setMedia", {
+            mp3: url
+          });
+          $("#player_1").jPlayer("play");
+        }
+        });
+      } else {
+        $(".play-track").removeAttr("data-playing");
+        track.closest("ul").find("li").removeClass("playing");
+        $("#player_1").jPlayer("stop");
+      }
+    }
   }
 });
